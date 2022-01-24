8a004cf64631369a06f316c5e0d15993294075a4
Ethan Hsu <ethanhsuhsu@gmail.com>
2022-01-24 01:07:31 -0800

    Added LocalFunctions and tests

---
+++ flare/src/commonMain/kotlin/enchant/flare/LocalFunctions.kt
@@ -0,0 +1,29 @@
+package enchant.flare
+
+import kotlinx.coroutines.TimeoutCancellationException
+import kotlinx.coroutines.withTimeout
+
+abstract class LocalFunctions : FirebaseFunctions {
+
+    abstract fun callFunction(name: String, data: Any?): Any?
+    override suspend fun call(name: String, data: Any?, timeout: Long?): Any? {
+        return if (timeout != null) {
+            try {
+                withTimeout(timeout) {
+                    callFunction(name, data)
+                }
+            } catch (e: TimeoutCancellationException) {
+                throw FunctionsException(
+                    FunctionsException.Code.DeadlineExceeded,
+                    "Function exceeded timeout: $timeout"
+                )
+            }
+        } else callFunction(name, data)
+    }
+
+    override val config: FirebaseFunctions.Config
+        get() = object : FirebaseFunctions.Config {
+            override fun useEmulator(host: String, port: Int) {
+            }
+        }
+}
---
+++ flare/src/commonTest/kotlin/FunctionsTest.kt
@@ -0,0 +1,27 @@
+import enchant.flare.FirebaseFunctions
+import enchant.flare.FunctionsException
+import enchant.flare.LocalFunctions
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class FunctionsTest : FlareTest() {
+
+    @Test
+    fun callFunction() = runTest {
+        val functions: FirebaseFunctions = if (useLocal) MyLocalFunctions()
+        else FirebaseFunctions.instance
+        val response = functions.call("increment",1)
+        assertEquals(2, response, "Ensure function incremented value")
+    }
+}
+
+private class MyLocalFunctions : LocalFunctions() {
+    override fun callFunction(name: String, data: Any?): Any? {
+        if (name == "increment") {
+            return (data as Int) + 1
+        } else {
+            throw FunctionsException(FunctionsException.Code.NotFound, "Function not found")
+        }
+    }
+
+}
