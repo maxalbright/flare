7552ed326e58ea5f1da85c71abb6fa61a1a13817
Max Albright <mdalbrig@usc.edu>
2022-03-31 15:02:05 -0700

    docs: added documentation to FirebaseFunctions.kt

--- flare/src/commonMain/kotlin/enchant/flare/FirebaseFunctions.kt
+++ flare/src/commonMain/kotlin/enchant/flare/FirebaseFunctions.kt
@@ -1,6 +1,17 @@
 package enchant.flare
 
+
+
 interface FirebaseFunctions {
+    /**
+    * Triggers Firebase cloud functions to run on the server
+    *
+    * @param name the name of the cloud function
+    * @param data the optional "input" to the function
+    * @param timeout the optional maximum time that the function is allowed to run 
+    *
+    * @throws FirebaseFunctionsException for errors thrown
+    */    
     suspend fun call(name: String, data: Any? = null, timeout: Long? = null): Any?
 
     interface Config {
@@ -13,6 +24,16 @@ interface FirebaseFunctions {
         fun getInstance(app: FirebaseApp) = getFunctionsInstance(app)
     }
 }
+
+
+/**
+* This class represents the type of error 
+* that Flare's FirebaseFunctions throws.
+*
+* @param code the code that is included in the error
+* @param description a string that provides a description of the error
+* in addition to code
+*/
 
 class FunctionsException(val code: Code, val description: String? = null) :
     Exception("Firebase functions operation failed with code ${code.name}: $description") {
